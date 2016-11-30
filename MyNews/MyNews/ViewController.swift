d5e7b32d6cedaf7a560a71d08196911010f3e45a
dawei.bai <452987573@qq.com>
2016-11-30 21:20:00 +0800

    初始化MyNews项目

---
+++ MyNews/MyNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyNews
+//
+//  Created by DF-Mac on 16/11/30.
+//  Copyright © 2016年 DF-Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
